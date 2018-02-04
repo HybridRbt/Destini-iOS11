fe33d1cde99a8710fdb98ac9301780bd534dfc1a
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2018-02-03 18:19:01 -0600

    add a number to record current story view

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var storyIndex : Int = 1
+    
     // Our strings
     let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
     let answer1a = "I\'ll hop in. Thanks for the help!"
